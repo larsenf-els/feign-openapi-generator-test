393366f7fab5601746c9a86f0e4a9af0fdf41595
Flemming Nørnberg Larsen <f.larsen@elsevier.com>
2024-05-17 11:17:26 +0200

    Simplified build script + bug fixed outputDir

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,7 @@ repositories {
 openApiGenerate {
     generatorName.set("java")
     inputSpec.set("$projectDir/src/main/resources/openapi.yaml")
-    outputDir.set("${layout.buildDirectory}/generated")
-    apiPackage.set("com.example.api")
-    modelPackage.set("com.example.model")
-    invokerPackage.set("com.example.invoker")
+    outputDir.set("${layout.buildDirectory.get()}/generated")
     configOptions.set(mapOf(
         "dateLibrary" to "java8",
         "library" to "feign"
